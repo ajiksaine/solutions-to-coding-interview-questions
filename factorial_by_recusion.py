08c4b0637430992d936d4722376c028ac81caed8
ajiksaine <ajiksaine@gmail.com>
2023-10-15 11:53:50 +0000

    Added description to solving the factorial problem

--- factorial_by_recusion.py
+++ factorial_by_recusion.py
@@ -5,7 +5,7 @@ def factorial(n):
     n! = n* (n-1) * (n-1) * n(n-3) ..... 3 * 2 * 1
     There for n! = n * (n-1)!
 
-    We know the factorial of = 1 which is our base case
+    We know the factorial of 0 = 1 which is our base case
 
     """
     if n == 0:
